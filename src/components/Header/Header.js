25e638b20df5cfa1bb7fd8fdf81929bccd6de484
jasimahmedq2 <jasimahmed90852417>
2022-04-04 00:02:52 +0600

    routing-setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <div className='anchor-parent'>
+          <Link to="/home">HOME</Link>
+          <Link to="/review">REVIEW</Link>
+          <Link to="/dashboard">DASHBOARD</Link>
+          <Link to="/blogs">BLOGS</Link>
+          <Link to="/about">ABOUT</Link>
+          
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
