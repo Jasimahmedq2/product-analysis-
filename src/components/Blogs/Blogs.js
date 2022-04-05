e1e75d79c1ac821434559076b5cff3d92b4eec6c
jasimahmedq2 <jasimahmed90852417>
2022-04-05 17:55:45 +0600

    blogs-components-complite

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
   return (
     <div>
       <h2>this is blogs page</h2>
-      <div className='d-flex'>
-        <div>
-        Context api is a way for a React app to effectively produce global variables.context api uses pass data evry components easily.
+      <div className='d-flex container'>
+        <div className='context-style shadow-lg bg-body '>
+          <h4>context Api</h4>
+        Context api is a way for a React app to effectively produce global variables.context api uses pass data evry components easily.Context api a separate storage space is created outside the Component tree. 
         
         </div>
-        <div>
-           writtin after some moments
+        <div className='semantic-style shadow-lg bg-body'>
+          <h4>semantic tag</h4>
+        Semantic HTML elements are those that clearly describe their meaning in a human and machine readable way.
+        For sighted user when a page is well designed visually, it is easy to identify the various parts of a web page at a glance.
         </div>
       </div>
     </div>
